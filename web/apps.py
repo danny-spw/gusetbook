ce7f90a7f46cc28a0f2787bba07d466458016aa3
10930278 <10930278@dcsh.tp.edu.tw>
2020-12-17 11:55:38 +0800

    登入登出

--- web/apps.py
+++ web/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class VebConfig(AppConfig):
-    name = 'veb'
+class WebConfig(AppConfig):
+    name = 'web'
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Message
+from django.urls import reverse_lazy
 # Create your views here.
 class MessageList(ListView):
     model = Message
@@ -13,3 +14,7 @@ class MessageCreate(CreateView):
     model = Message
     fields = '__all__' 
     success_url = reverse_lazy('msg_list')
+
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')          
